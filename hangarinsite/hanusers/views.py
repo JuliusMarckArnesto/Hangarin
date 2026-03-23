4e51476502d9e08092c4fc5a458c0fa08b2ff503
JuliusMarckArnesto <jmarnestso@gmail.com>
2026-03-23 11:26:02 +0800

    created hompage

--- hangarinsite/hanusers/views.py
+++ hangarinsite/hanusers/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Task, Note
 
-# Create your views here.
+def dashboard_view(request):
+    # Fetching 3 notes for the dashboard cards
+    recent_notes = Note.objects.all()[:3]
+    # Fetching all tasks
+    tasks = Task.objects.select_related('priority', 'category').all()
+    
+    return render(request, 'dashboard.html', {
+        'notes': recent_notes,
+        'tasks': tasks
+    })
