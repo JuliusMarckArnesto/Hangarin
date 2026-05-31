476e23e87c6f2daa9d6ee83786c0dd5f3f923da9
JuliusMarckArnesto <jmarnestso@gmail.com>
2026-05-31 19:19:43 +0800

    Added list view

--- projectsite/hangarinapp/views.py
+++ projectsite/hangarinapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from hangarinapp.models import Task
+from hangarinapp.models import Task, Note, SubTask, Category, Priority
 
 class TaskListView(ListView):
     model = Task
-    context_object_name = 'taskpage'
-    template_name = 'main_page.html'
+    context_object_name = 'tasklist'
+    template_name = 'task_list.html'
+    paginate_by = None
