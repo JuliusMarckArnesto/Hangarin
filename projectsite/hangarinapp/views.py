44eb1d8ffa5370725d71320d1dd0431da93842c9
JuliusMarckArnesto <jmarnestso@gmail.com>
2026-06-02 10:54:44 +0800

    added functional search and filter

--- projectsite/hangarinapp/views.py
+++ projectsite/hangarinapp/views.py
@@ -6,11 +6,75 @@ from hangarinapp.forms import TaskForm, NoteForm, SubTaskForm
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404, redirect
 from django.views import View
+from django.db.models import Q
+from django.utils import timezone
+
 class TaskListView(ListView):
     model = Task
     context_object_name = 'tasklist'
     template_name = 'task_list.html'
     paginate_by = None
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        #---SEARCH---
+        query = self.request.GET.get('q')
+        if query:
+            qs =qs.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
+            )
+
+        #---SORTING---
+        sort_options = {
+            'deadline':  'deadline',
+            '-deadline': '-deadline',
+            'title':     'title',
+            '-title':    '-title',
+            'status':    'status',
+            'priority':  'priority__priority_level',
+            'category':  'category__category_name',
+            'created':   'created_at',
+            '-created':  '-created_at',
+        }
+        sort_by = self.request.GET.get('sort_by', 'deadline') # sort from templates
+        order = sort_options.get(sort_by, 'deadline')# sort from here
+        qs = qs.order_by(order)
+
+        return qs
+    
+        #---CONTEXT---
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_tasks'] = Task.objects.count()
+
+        today = timezone.now().date()
+
+        count = (
+            Task.objects.filter(created_at__year=today.year).count(),
+            Task.objects.filter(created_at__month=today.month).count(),
+        )
+        context['tasks_this_year'] = count[0]
+        context['task_this_month'] = count[1]
+
+        context['task_pending'] = Task.objects.filter(status="Pending").count()
+        context['task_inprogress'] = Task.objects.filter(status="In Progress").count()
+        context['task_completed'] = Task.objects.filter(status="Completed").count()
+
+        context['priority_high'] = Task.objects.filter(priority__priority_level="high").count()
+        context['priority_medium'] = Task.objects.filter(priority__priority_level="medium").count()
+        context['priority_low'] = Task.objects.filter(priority__priority_level="low").count()
+        context['priority_optional'] = Task.objects.filter(priority__priority_level="optional").count()
+
+        context['category_work'] = Task.objects.filter(category__category_name="Work").count()
+        context['category_school'] = Task.objects.filter(category__category_name="School").count()
+        context['category_personal'] = Task.objects.filter(category__category_name="Personal").count()
+        context['category_optional'] = Task.objects.filter(category__category_name="Optional").count()
+
+        context['overdue_tasks'] = Task.objects.filter(deadline__lt=today).exclude(status='Completed').count()
+        context['due_soon'] = Task.objects.filter(deadline__range=(today, today + timezone.timedelta(hours=24))).exclude(status='Completed').count()
+        return context
 
 class TaskCreateView(CreateView):
     model = Task
@@ -35,6 +99,45 @@ class NoteListView(ListView):
     context_object_name = "notelist"
     template_name = "note_list.html"
     paginate_by = None
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            qs = qs.filter(
+                Q(task__title__icontains=query) |
+                Q(content__icontains=query)
+            )
+
+        #---SORTING---
+        sort_options = {
+            'content':  'content',
+            '-content': '-content',
+            'title':     'task__title',
+            '-title':    '-task__title',
+            'created':   'created_at',
+            '-created':  '-created_at',
+            'updated':   'updated_at',
+            '-updated':  '-updated_at',
+        }
+        sort_by = self.request.GET.get('sort_by', '-created')
+        order = sort_options.get(sort_by, '-created')
+        qs = qs.order_by(order)
+
+        return qs
+    #---CONTEXT---
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_notes'] = Note.objects.count()
+
+        today = timezone.now().date()
+        count = (
+            Note.objects.filter(created_at__year=today.year).count(),
+            Note.objects.filter(created_at__month=today.month).count(),
+        )
+        context['notes_this_year'] = count[0]
+        context['notes_this_month'] = count[1]
+        return context
 
 class NoteNoteTaskModal(ListView):
     model = Note
