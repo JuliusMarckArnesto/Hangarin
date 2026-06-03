67c62db9cdd4a7f6fb6f5a01c15744ec249cab51
JuliusMarckArnesto <jmarnestso@gmail.com>
2026-06-03 08:56:45 +0800

    allowed the webserver

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%m16yj5nst51t_z*owkag6wfebm7pnve3bnopw#i-5c5qiyxq^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['micro.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
